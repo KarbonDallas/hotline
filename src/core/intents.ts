7a32410fbd5503b952d573e3d007a2f779aac7be
Karbon Dallas <617235+KarbonDallas@users.noreply.github.com>
2025-02-06 23:09:03 -0800

    Removing Discord bot stuff for now

--- src/core/intents.ts
+++
@@ -1,8 +0,0 @@
-import { GatewayIntentBits } from 'discord.js'
-type Intents = GatewayIntentBits[]
-const intents: Intents = [
-	GatewayIntentBits.Guilds,
-	GatewayIntentBits.GuildMessages,
-	GatewayIntentBits.GuildMembers,
-]
-export default intents
