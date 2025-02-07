361ccaede87e2fd5cf4924eb3eebd64d516c7817
Karbon Dallas <617235+KarbonDallas@users.noreply.github.com>
2025-02-07 00:28:02 -0800

    Remove old config.ts

--- config.ts
+++
@@ -1,8 +0,0 @@
-type Config = {
-	discord_token: string
-}
-const config: Config = {
-	discord_token:
-		'MTE3OTUzMDcwMTUzNjc1NTg3Mw.GOOwL3.-JwUREynHaCLhlcs4vlbUoWygNirjK_9f5OSmY',
-}
-export default config
